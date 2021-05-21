8d13c9b8046e19570578a5fe27569b066aa76cc5
Dan Whitford <dan.whitford@fanduel.com>
2021-05-21 14:25:07 +0100

    The rest of the owl

---
+++ README.md
@@ -0,0 +1,3 @@
+Yet Another Sprint Namer
+
+See at https://shaftoe44.github.io/sprint-namr/
--- myscript.js
+++ myscript.js
@@ -1,31 +1,68 @@
-import adjectives from './rlp-adj.js'
-import animals from './rlp-animals.js'
+import adjectives from "./rlp-adj.js";
+import animals from "./rlp-animals.js";
 
 function randomWord(array) {
-    return array[Math.floor(Math.random() * array.length)];
+  return array[Math.floor(Math.random() * array.length)];
 }
 
 const capitalize = (s) => {
-    if (typeof s !== 'string') return ''
-    return s.charAt(0).toUpperCase() + s.slice(1).toLocaleLowerCase()
-  }
+  if (typeof s !== "string") return "";
+  return s
+    .split(" ")
+    .map((w) => w.charAt(0).toUpperCase() + w.slice(1).toLocaleLowerCase())
+    .join(" ");
+};
 
 function transition(n, divId, words) {
-    var array = location.hash
-        ? words.filter(w => w.startsWith(location.hash.substr(1, 1)))
-        : words
+  var array = location.hash
+    ? words.filter((w) => w.startsWith(location.hash.substr(1, 1)))
+    : words;
 
-    setTimeout(() => {
-        document.getElementById(divId).innerHTML = capitalize(randomWord(array))
-    }, n)
+  setTimeout(() => {
+    document.getElementById(divId).innerHTML = capitalize(randomWord(array));
+  }, n);
 }
 
-document.getElementById('btn').onclick = function () {
-    for (var i = 1000; i > 0; i -= (Math.random() * 50)) {
-        transition(i, 'adj', adjectives);
-    }
+function spin() {
+  const ph = document.getElementById("placeholder");
+  if (ph) ph.remove();
 
-    for (var i = 1000; i > 0; i -= (Math.random() * 50)) {
-        transition(i, 'animal', animals);
-    }
+  for (var i = 1000; i > 0; i -= Math.random() * 50) {
+    transition(i, "adj", adjectives);
+    transition(i, "animal", animals);
+  }
+
+  setTimeout(() => {
+    document.getElementById("cpy-icon").className = "";
+  }, 1000);
 }
+
+document.getElementById("btn").onclick = spin;
+
+document.getElementById("cpy-btn").onclick = function () {
+  const el = document.createElement("textarea");
+  el.value =
+    document.getElementById("adj").innerText +
+    " " +
+    document.getElementById("animal").innerText;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+
+  document.execCommand("copy");
+
+  const btn = document.getElementById("cpy-icon");
+  btn.src = "static/images/check.svg";
+  setTimeout(() => {
+    btn.src = "static/images/copy.svg";
+  }, 1000);
+};
+
+document.onkeypress = function (ev) {
+  if (ev.key.match(/^[a-z]$/i)) {
+    location.hash = ev.key.toLocaleUpperCase();
+  } else if (ev.key === " " || ev.key === "Enter") {
+    spin();
+  }
+};
